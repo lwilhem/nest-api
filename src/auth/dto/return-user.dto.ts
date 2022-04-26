4194878b0254ba53c82056566471e0fa4dfa7788
lwilhem <59793152+lwilhem@users.noreply.github.com>
2022-04-26 20:57:55 +0200

    :hammer: migrations

--- src/auth/dto/return-user.dto.ts
+++ src/auth/dto/return-user.dto.ts
@@ -1,25 +1,15 @@
 import { Role } from '@prisma/client';
-import { Exclude } from 'class-transformer';
-import { IsEmail, IsEnum, IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class ReturnUserDto {
-  @IsNotEmpty()
-  @IsInt()
   id: number;
 
-  @IsNotEmpty()
-  @IsEmail()
   email: string;
 
-  @IsNotEmpty()
-  @IsString()
   username: string;
 
-  @IsString()
-  @Exclude()
   password: string;
 
-  @IsNotEmpty()
-  @IsEnum(Role)
+  avatar?: string;
+
   role: Role;
 }
