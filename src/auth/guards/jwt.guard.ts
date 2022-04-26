3c22f012b66e10da8e07b821a97e5d9363fe21cb
lwilhem <59793152+lwilhem@users.noreply.github.com>
2022-04-26 14:30:42 +0200

    :rocket: Role in JwtGuards

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,5 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Role } from '@prisma/client';
+import { ROLES_KEY } from '../decorators/role.decorator';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') implements CanActivate {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+    const { user } = context.switchToHttp().getRequest();
+    return requiredRoles.some((role) => user.roles?.includes(role));
+  }
+}
