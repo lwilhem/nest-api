36dd3e321840a92415a0ce30c845a6bc9181455f
lwilhem <59793152+lwilhem@users.noreply.github.com>
2022-04-28 18:01:40 +0200

    :rocket: file upload

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -7,10 +7,14 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiTags } from '@nestjs/swagger';
+import { Role } from '@prisma/client';
 import { diskStorage } from 'multer';
 import { of } from 'rxjs';
 import { v4 as uuidv4 } from 'uuid';
+import { Roles } from '../auth/decorators/role.decorator';
 import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { RolesGuard } from '../auth/guards/role.guard';
 import { FileService } from './file.service';
 import path = require('path');
 
@@ -23,18 +27,51 @@ export const avatarStorage = {
       const extension: string = path.parse(file.originalname).ext;
 
       cb(null, `${filename}${extension}`);
+      console.log(req);
+    },
+  }),
+};
+
+export const shopStorage = {
+  storage: diskStorage({
+    destination: './public/uploads/shops',
+    filename: (req, file, cb) => {
+      const filename: string =
+        path.parse(file.originalname).name.replace(/\s/g, '') + uuidv4();
+      const extension: string = path.parse(file.originalname).ext;
+
+      cb(null, `${filename}${extension}`);
+      console.log(req);
     },
   }),
 };
 
 @Controller('file')
+@ApiTags('Uploads de Fichiers')
 export class FileController {
   constructor(private fileService: FileService) {}
 
   @UseGuards(JwtAuthGuard)
   @Post('avatars/upload')
   @UseInterceptors(FileInterceptor('avatar', avatarStorage))
-  async uploadFile(@UploadedFile() file: any, @Request() req: any) {
+  async uploadAvatar(@UploadedFile() file: any, @Request() req: any) {
+    console.log(req.user);
+    return this.fileService.avatarHandler(file, req.user);
+  }
+
+  @Roles(Role.RETAILER, Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Post('shops/upload')
+  @UseInterceptors(FileInterceptor('avatar', avatarStorage))
+  async uploadShop(@UploadedFile() file: any, @Request() req: any) {
+    console.log(req.user);
+    return of(file);
+  }
+
+  @Roles(Role.RETAILER, Role.ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Post('products/uploads')
+  async uploadProducts(@UploadedFile() file: any, @Request() req: any) {
     console.log(req.user);
     return of(file);
   }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,7 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { of } from 'rxjs';
 import { PrismaService } from '../database/database.service';
 
 @Injectable()
 export class FileService {
   constructor(private prisma: PrismaService) {}
+
+  async avatarHandler(file: any, req: any) {
+    const user = await this.prisma.user.findUnique({ where: { id: req.id } });
+    if (!user) throw new UnauthorizedException();
+
+    try {
+      await this.prisma.user.update({
+        where: { id: user.id },
+        data: { avatar: file.destination, path: file.directory },
+      });
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
+
+    return of(file);
+  }
 }
